87ddcac40d1f24605cd078e0b70521168f3e68ba
Anand A <anand.mudhaliar@gmail.com>
2025-07-15 01:40:10 +0530

    removed parkour

--- Assets/Fantacode Studios/Third Person Controller/Equip And Attach/Scripts/Equip/ThrowableItemData.cs
+++
@@ -1,8 +0,0 @@
-namespace FS_ThirdPerson
-{
-    public class ThrowableItemData : EquippableItem
-    {
-        public AnimGraphClipInfo aimAnimation;
-        public AnimGraphClipInfo throwAnimation;
-    }
-}
